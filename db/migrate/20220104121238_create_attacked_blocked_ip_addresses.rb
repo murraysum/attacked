be936c80c8ee3ae623751b66d2a5e67a95bde538
Murray Summers <murray@tito.io>
2022-01-04 16:05:07 +0000

    Use version 5.1 for the migration so that we can use the migration in older versions of rails.

--- db/migrate/20220104121238_create_attacked_blocked_ip_addresses.rb
+++ db/migrate/20220104121238_create_attacked_blocked_ip_addresses.rb
@@ -1,4 +1,4 @@
-class CreateAttackedBlockedIpAddresses < ActiveRecord::Migration[6.1]
+class CreateAttackedBlockedIpAddresses < ActiveRecord::Migration[5.1]
   def change
     create_table :attacked_blocked_ip_addresses do |t|
       t.string :ip_address, index: { unique: true }
