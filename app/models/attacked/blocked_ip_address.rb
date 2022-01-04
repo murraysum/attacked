35e40876ec15c97867c53421e6e2b7a16c205e9c
Murray Summers <murray@tito.io>
2022-01-04 12:28:59 +0000

    When a block ip address is created it is pushed it is cached for quick lookups.

--- app/models/attacked/blocked_ip_address.rb
+++ app/models/attacked/blocked_ip_address.rb
@@ -1,4 +1,49 @@
+require "resolv"
+
 module Attacked
   class BlockedIpAddress < ApplicationRecord
+
+    after_create :create_in_cache
+
+    after_destroy :remove_from_cache
+
+    validates :ip_address, format: {
+      with: Regexp.union(Resolv::IPv4::Regex, Resolv::IPv6::Regex)
+    }
+
+    def self.blocked?(ip_address)
+      Rails.cache.read(blocked_cache_key(ip_address)) ? true : false
+    end
+
+    def self.block(ip_address)
+      create ip_address: ip_address
+    end
+
+    def self.unblock(ip_address)
+      blocked_ip_address = find_by(ip_address: ip_address)
+      blocked_ip_address.unblock
+    end
+
+    def unblock
+      destroy
+    end
+
+    private
+
+    def create_in_cache
+      Rails.cache.write(blocked_cache_key(ip_address), true)
+    end
+
+    def remove_from_cache
+      Rails.cache.delete(blocked_cache_key(ip_address))
+    end
+
+    def self.blocked_cache_key(ip_address)
+      "attacked_blocked_ip #{ip_address}"
+    end
+
+    def blocked_cache_key(ip_address)
+      "attacked_blocked_ip #{ip_address}"
+    end
   end
 end
