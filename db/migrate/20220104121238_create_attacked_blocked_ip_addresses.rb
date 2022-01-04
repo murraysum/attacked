f9493773ab9d96260141482fe5cabfbba09f169e
Murray Summers <murray@tito.io>
2022-01-04 13:01:22 +0000

    Add a unique index to the ip address column and add a description field so that it is easier to understand why an ip has been blocked.

--- db/migrate/20220104121238_create_attacked_blocked_ip_addresses.rb
+++ db/migrate/20220104121238_create_attacked_blocked_ip_addresses.rb
@@ -1,7 +1,8 @@
 class CreateAttackedBlockedIpAddresses < ActiveRecord::Migration[6.1]
   def change
     create_table :attacked_blocked_ip_addresses do |t|
-      t.string :ip_address
+      t.string :ip_address, index: { unique: true }
+      t.text :description
       t.timestamps
     end
   end
