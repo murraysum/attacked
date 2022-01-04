599df446dcf170009417e64e3a27b0f2191e1566
Murray Summers <murray@tito.io>
2022-01-04 16:52:56 +0000

    Add test file to check the operation of blocking and unblocking an ip address, the format of the ip address.

--- test/models/attacked/blocked_ip_address_test.rb
+++ test/models/attacked/blocked_ip_address_test.rb
@@ -2,8 +2,40 @@ require "test_helper"
 
 module Attacked
   class BlockedIpAddressTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+
+    test "validates an ip address is present" do
+      blocked_ip_address = BlockedIpAddress.new
+      assert blocked_ip_address.invalid?
+
+      assert_equal ["is invalid"], blocked_ip_address.errors[:ip_address]
+    end
+
+    test "validates the format of the ip address" do
+      blocked_ip_address = BlockedIpAddress.new ip_address: "1.a.2b.3"
+      assert blocked_ip_address.invalid?
+
+      assert_equal ["is invalid"], blocked_ip_address.errors[:ip_address]
+    end
+
+    test "blocking an ip address" do
+      ip_address = "1.2.3.4"
+
+      refute BlockedIpAddress.blocked?(ip_address)
+
+      BlockedIpAddress.block(ip_address)
+
+      assert BlockedIpAddress.blocked?(ip_address)
+    end
+
+    test "unblocking an ip address" do
+      ip_address = "1.2.3.4"
+      BlockedIpAddress.block(ip_address)
+
+      assert BlockedIpAddress.blocked?(ip_address)
+
+      BlockedIpAddress.unblock(ip_address)
+
+      refute BlockedIpAddress.blocked?(ip_address)
+    end
   end
 end
